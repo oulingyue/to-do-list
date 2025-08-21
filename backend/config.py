a413c41094f0c0d6336fef0b0b90b61a24efc1b8
graceou <oulingyue@gmail.com>
2025-08-20 21:59:47 -0400

    gitting rid of SQLAlchemy references and model class in the backend

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,3 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
